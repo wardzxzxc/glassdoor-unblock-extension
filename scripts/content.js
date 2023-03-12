f8020fd7b1a484602e05f831fc09743436f9346c
Edward Wang <wardzxzxc@gmail.com>
2023-03-12 23:34:39 +0800

    first commit

---
+++ scripts/content.js
@@ -0,0 +1,6 @@
+document.body.style.overflow = "auto";
+document.body.style.position = null;
+console.log('test')
+
+var overlay = document.getElementById("HardsellOverlay");
+overlay.parentElement.removeChild(overlay);
