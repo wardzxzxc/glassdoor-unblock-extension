79a8433291335b5f52e71b54b0b55a691bb268f0
Edward Wang <wardzxzxc@gmail.com>
2023-05-06 22:00:28 +0800

    update: use css to manipulate dom, added new domains

--- scripts/content.js
+++
@@ -1,5 +0,0 @@
-document.body.style.overflow = "auto";
-document.body.style.position = null;
-
-var overlay = document.getElementById("HardsellOverlay");
-overlay?.parentElement.removeChild(overlay);
